da6c4f67ad7d7f904362962efcfcbb934e2b3168
DeclanLacey <declan.lacey@hotmail.com>
2024-09-15 11:11:07 -0500

    Created files for components needed for the overview page

---
+++ src/components/balancesOveriew/BalancesOverview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BalancesOverview() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/transactionsOverview/TransactionsOverview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TransactionsOverview() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
