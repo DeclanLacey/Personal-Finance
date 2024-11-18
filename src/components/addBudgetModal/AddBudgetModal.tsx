6a716fcf10e242a1b46e20e297ef1bc70b9cbe10
DeclanLacey <declan.lacey@hotmail.com>
2024-11-18 11:47:33 -0600

    addBudgetModal styling

--- src/components/addBudgetModal/AddBudgetModal.tsx
+++ src/components/addBudgetModal/AddBudgetModal.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react"
 import "./AddBudgetModal.css"
-import { Combobox, ComboboxOption, FieldWrap } from '@deque/cauldron-react'
 import { Theme } from "../../types/types"
 import { getThemes } from "../../utils/clientCalls"
-import '@deque/cauldron-styles'; 
-import { updatePassword } from "aws-amplify/auth"
 
 interface Props {
     setShowAddBudgetModal: Function,
@@ -43,17 +40,11 @@ export default function AddBudgetModal({setShowAddBudgetModal, renderCategoryNam
 
     function renderColorOptions() {
         const colorOptions = themes?.map((theme, index) => {
-            let upperCaseName = theme.name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()}) as string
+            let upperCaseName : string = theme.name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()}) 
             return (
-                <div className="combobox-container" key={index}>
-                    <span className="combobox-color-tag-color"></span>
-                    
-                    <ComboboxOption className="color-tag-input" value={theme.name}> </ComboboxOption>
-                </div>
+                <option className={`color-option`} value={theme.name}>{upperCaseName}</option>
             )
         })
-
-        console.log(colorOptions)
         
         return colorOptions
     }
@@ -63,13 +54,13 @@ export default function AddBudgetModal({setShowAddBudgetModal, renderCategoryNam
             <div className='page-cover'></div>
             <section className='add-modal'>
                 <div className="add_budget-title-container">
-                    <h2 className="add_budget-title">Add New Transaction</h2>
+                    <h2 className="add_budget-title">Add New Budget</h2>
                     <img className="close-modal-btn" onClick={() => setShowAddBudgetModal(false)} src="./assets/icon-close-modal.svg" />
                 </div>
 
                 <form>
 
-                    {/* <div className="add_budget-amount-container">
+                    <div className="add_budget-amount-container">
                         <label className="add-modal-input-label">Amount</label>
                         <span className="dollar-sign">$</span>
                         <input required name="amount" maxLength={9} placeholder="e.g 49.99" className="rounded-input amount-input" />
@@ -81,36 +72,18 @@ export default function AddBudgetModal({setShowAddBudgetModal, renderCategoryNam
                             <option value="">-- Select Category</option>
                             {renderCategoryNameOptions()}
                         </select>
-                    </div> */}
+                    </div>
 
-                    {/* <div className="add_budget-input-container">
+                    <div className="add_budget-input-container">
                         <label className="add-modal-input-label">Color Tag</label>
                         <select required name="category" className="rounded-select-input">
                             <option value="">-- Select Color</option>
-                            
-                                <option> 
-                                    <div className="color-circle">
-
-                                    </div>
-                                    Green
-                                </option>
-                           
-                            
+                            {renderColorOptions()}
                         </select>
-                        
-                    </div> */}
+                    </div>
 
+                    <input type="submit" className="black-add-btn add-budget-btn" value={"Add Budget"}></input>
 
-{/*     
-                    <FieldWrap className="input-field-wrap">
-                        <Combobox label="Color Tag">
-                             {renderColorOptions()}
-                        </Combobox>
-                    </FieldWrap> */}
-
-                    
-                    
-                  
                 </form>
             </section>
         </>
