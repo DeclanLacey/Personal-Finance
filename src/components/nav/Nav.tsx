f6fbcc03befc71f036c0783395e63b46ea746561
DeclanLacey <declan.lacey@hotmail.com>
2024-11-27 10:00:30 -0600

    Responsive styling

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -6,22 +6,27 @@ export default function Nav() {
     <nav className='nav-container'>
         <NavLink className='nav-link' to={"/overview"}> 
             <img alt='representing the navigation element that can be clicked to go to the overview page' className='nav-link-img' src='/assets/icon-nav-overview.svg' />
+            <p className='nav-text text-5-bold-grey'>Overview</p>
         </NavLink>
 
         <NavLink className='nav-link' to={"/transactions"}> 
             <img alt='representing the navigation element that can be clicked to go to the transactions page' className='nav-link-img' src='/assets/icon-nav-transactions.svg' />
+            <p className='nav-text text-5-bold-grey'>Transactions</p>
         </NavLink>
 
         <NavLink className='nav-link' to={"/budgets"}> 
             <img alt='representing the navigation element that can be clicked to go to the budgets page' className='nav-link-img' src='/assets/icon-nav-budgets.svg' />
+            <p className='nav-text text-5-bold-grey'>Budgets</p>
         </NavLink>
 
         <NavLink className='nav-link' to={"/pots"}> 
             <img alt='representing the navigation element that can be clicked to go to the pots page' className='nav-link-img' src='/assets/icon-nav-pots.svg' />
+            <p className='nav-text text-5-bold-grey'>Pots</p>
         </NavLink>
 
         <NavLink className='nav-link' to={"/recurring-bills"}> 
             <img alt='representing the navigation element that can be clicked to go to the recurring bills page' className='nav-link-img' src='/assets/icon-nav-recurring-bills.svg' />
+            <p className='nav-text text-5-bold-grey'>Recurring Bills</p>
         </NavLink>
     </nav>
   )
